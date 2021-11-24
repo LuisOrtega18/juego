a869f806cdb401ac182ec6d257c289ad61663280
LuisOrtega18 <lortegas1900@alumno.ipn.mx>
2021-11-24 02:17:54 -0600

    inicio del sitio web

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO (server);
+
+
+app.set('port', process.env.PORT || 3000);
+
+
+require("./sockets")(io);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+server.listen(app.get ('port'), () => {
+    console.log(`Server on port 300`);
+});
